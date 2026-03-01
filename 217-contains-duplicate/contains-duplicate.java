7d1cd7c900cb3f56fd44a49d605d31ba5b41f6a7
Rani Aghav <raniaghav07@gmail.com>
2026-03-01 19:03:18 +0530

    Time: 14 ms (86.72%) | Memory: 93.3 MB (63.76%) - LeetSync

---
+++ 217-contains-duplicate/contains-duplicate.java
@@ -0,0 +1,14 @@
+import java.util.HashSet;
+class Solution {
+    public boolean containsDuplicate(int[] nums) {
+       HashSet<Integer>set=new HashSet<>();
+       for(int number:nums){
+        if(set.contains(number)){
+            return true;
+        }
+        set.add(number);
+
+       }
+       return false;
+    }
+}
